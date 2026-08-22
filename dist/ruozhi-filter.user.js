925006f2bc315223d701153bdad072f941733564
ruanshining <ruanshining@outlook.com>
2026-08-22 23:47:21 +0800

    chore: bump to v0.6.0

--- dist/ruozhi-filter.user.js
+++ dist/ruozhi-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         信息茧房放大器 - B站降智评论过滤器
 // @namespace    ruozhi-filter
-// @version      0.5.0
+// @version      0.6.0
 // @author       ruozhi-filter
 // @description  AI驱动：自动识别并折叠B站评论区中的降智/引战言论
 // @license      MIT
